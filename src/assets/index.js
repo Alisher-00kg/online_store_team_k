b237b9af0208c621df3b19f66575c44703ecd998
daniyar-bro <daniarakmatkulov6@gmail.com>
2025-05-15 16:20:04 +0300

    media

---
+++ src/assets/index.js
@@ -0,0 +1,54 @@
+import ArrowBottom from "./icons/arrowBottom.svg";
+import ArrowLeft from "./icons/arrowLeft.svg";
+import ArrowRight from "./icons/arrowRight.svg";
+import ArrowTop from "./icons/arrowTop.svg";
+import Basket from "./icons/basket.svg";
+import BrandName from "./icons/brandName.svg";
+import Change from "./icons/change.svg";
+import Close from "./icons/close.svg";
+import ColorChange from "./icons/colorChange.svg";
+import Copy from "./icons/copy.svg";
+import Delete from "./icons/delete.svg";
+import FavoriteLike from "./icons/favoriteLike.svg";
+import Favorites from "./icons/favorites.svg";
+import Filters from "./icons/filters.svg";
+import Heart from "./icons/heart.svg";
+import ImageAdd from "./icons/imageAdd.svg";
+import Kid from "./icons/kid.svg";
+import Login from "./icons/login.svg";
+import LoginInput from "./icons/loginInput.svg";
+import Man from "./icons/man.svg";
+import Warning from "./icons/warning.svg";
+import Woman from "./icons/woman.svg";
+import Zoom from "./icons/zoom.svg";
+import GreenPlus from "./icons/greenPlus.svg";
+import Plus from "./icons/plus.svg";
+import Minus from "./icons/minus.svg";
+export const icons = {
+  ArrowBottom,
+  ArrowLeft,
+  ArrowRight,
+  ArrowTop,
+  Basket,
+  BrandName,
+  Change,
+  Close,
+  ColorChange,
+  Copy,
+  Delete,
+  FavoriteLike,
+  Favorites,
+  Filters,
+  Heart,
+  ImageAdd,
+  Kid,
+  Login,
+  LoginInput,
+  Man,
+  Warning,
+  Woman,
+  Zoom,
+  GreenPlus,
+  Plus,
+  Minus,
+};
