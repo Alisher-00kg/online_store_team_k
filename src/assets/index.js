57e96f1550c7a898483701ee13c81e53b571c805
daniyar-bro <daniarakmatkulov6@gmail.com>
2025-05-15 18:23:57 +0300

    media

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,31 +1,39 @@
-import ArrowBottom from "./icons/arrowBottom.svg";
-import ArrowLeft from "./icons/arrowLeft.svg";
-import ArrowRight from "./icons/arrowRight.svg";
-import ArrowTop from "./icons/arrowTop.svg";
-import Basket from "./icons/basket.svg";
-import BrandName from "./icons/brandName.svg";
-import Change from "./icons/change.svg";
-import Close from "./icons/close.svg";
-import ColorChange from "./icons/colorChange.svg";
-import Copy from "./icons/copy.svg";
-import Delete from "./icons/delete.svg";
-import FavoriteLike from "./icons/favoriteLike.svg";
-import Favorites from "./icons/favorites.svg";
-import Filters from "./icons/filters.svg";
-import Heart from "./icons/heart.svg";
-import ImageAdd from "./icons/imageAdd.svg";
-import Kid from "./icons/kid.svg";
-import Login from "./icons/login.svg";
-import LoginInput from "./icons/loginInput.svg";
-import Man from "./icons/man.svg";
-import Warning from "./icons/warning.svg";
-import Woman from "./icons/woman.svg";
-import Zoom from "./icons/zoom.svg";
-import GreenPlus from "./icons/greenPlus.svg";
-import Plus from "./icons/plus.svg";
-import Minus from "./icons/minus.svg";
-import Beige from "./icons/beige.svg";
-import Blue from "./icons/blue.svg";
+export { default as BlueSet } from "./images/blueSet.png";
+export { default as GreenSet } from "./images/greenSet.png";
+export { default as Shorts } from "./images/shorts.png";
+export { default as Top } from "./images/top.png";
+export { default as WhiteSet } from "./images/whiteSet.png";
+export { default as ZebraDess } from "./images/zebraDess.png";
+
+import ArrowBottom from "./icons/arrowBottom.svg?react";
+import ArrowLeft from "./icons/arrowLeft.svg?react";
+import ArrowRight from "./icons/arrowRight.svg?react";
+import ArrowTop from "./icons/arrowTop.svg?react";
+import Basket from "./icons/basket.svg?react";
+import BrandName from "./icons/brandName.svg?react";
+import Change from "./icons/change.svg?react";
+import Close from "./icons/close.svg?react";
+import ColorChange from "./icons/colorChange.svg?react";
+import Copy from "./icons/copy.svg?react";
+import Delete from "./icons/delete.svg?react";
+import FavoriteLike from "./icons/favoriteLike.svg?react";
+import Favorites from "./icons/favorites.svg?react";
+import Filters from "./icons/filters.svg?react";
+import Heart from "./icons/heart.svg?react";
+import ImageAdd from "./icons/imageAdd.svg?react";
+import Kid from "./icons/kid.svg?react";
+import Login from "./icons/login.svg?react";
+import LoginInput from "./icons/loginInput.svg?react";
+import Man from "./icons/man.svg?react";
+import Warning from "./icons/warning.svg?react";
+import Woman from "./icons/woman.svg?react";
+import Zoom from "./icons/zoom.svg?react";
+import GreenPlus from "./icons/greenPlus.svg?react";
+import Plus from "./icons/plus.svg?react";
+import Minus from "./icons/minus.svg?react";
+import Beige from "./icons/beige.svg?react";
+import Blue from "./icons/blue.svg?react";
+
 export const icons = {
   ArrowBottom,
   ArrowLeft,
